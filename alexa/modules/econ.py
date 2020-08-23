f6190c3b529cbec086a75e679c595da331499f85
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 09:52:47 +0000

    Delete econ.py

--- alexa/modules/econ.py
+++
@@ -1,7 +0,0 @@
-__help__ = """
- - /connect | /connect <chatid> in PM: connects a chat to PM 
- - /connection: list connected chats 
- - /disconnect: disconnect from a chat
- - /helpconnect: list available commands that can be done remotely
-""" 
-__mod_name__ = "Connection🆗"
