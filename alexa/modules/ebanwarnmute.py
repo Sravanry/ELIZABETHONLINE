08fd147063945b3258f5cad763c6425acd212580
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 07:30:10 +0000

    Create ebanwarnmute.py

---
+++ alexa/modules/ebanwarnmute.py
@@ -0,0 +1,21 @@
+__help__ = """
+*Ban*
+ - /ban: bans a user 
+ - /tban <d/h/m> : temporarily bans a user from your chat
+ - /unban: unbans a user 
+ - /sban: silently bans a user
+ - /kick: kicks a user
+ - /mute: mute a user
+*Mute*
+ - /tmute <d/h/m>: temporarily mute a user
+ - /unmute: unmutes a user
+*Warn*
+ - /warn <userhandle>: warn a user
+ - /resetwarn @username: reset the warnings for a user
+ - /addwarn <word> <message>: set a warning filter on a certain word
+ - /nowarn <word>: stop a warning filter
+ - /warnlimit <num>: set the max warning limit
+ - /warns <userhandle>: get a user's number, and reason, of warnings
+ - /warnlist: list of all current warning filters
+
+__mod_name__ = "B/M/W🚫"
