34b2edc7212ad64d8a311a3670309716bb16e41b
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 09:25:47 +0000

    Create eblack.py

---
+++ alexa/modules/eblack.py
@@ -0,0 +1,8 @@
+__help__ = """
+ - /addblacklist <trigger> : blacklists the trigger it will get removed everytime someone types it
+ - /unblacklist <trigger> | /rmblacklist <trigger> : stop blacklisting a certain blacklist trigger
+ - /blacklist: list all active blacklist filters
+ - /addblacklist "the admins suck": This will remove the text everytime someone types it
+ - /addblacklist "bit.ly/*": This will remove the link everytime someone sends it matching bit.ly
+""" 
+__mod_name__ = "BlackLst😡"
