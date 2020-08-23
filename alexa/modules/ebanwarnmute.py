3d720d1987502493e877f3de914fdb6ddc8833be
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 08:31:35 +0000

    Update ebanwarnmute.py

--- alexa/modules/ebanwarnmute.py
+++ alexa/modules/ebanwarnmute.py
@@ -1,15 +1,12 @@
 __help__ = """
- Ban cmds
  - /ban: bans a user 
  - /tban <d/h/m> : temporarily bans a user from your chat
  - /unban: unbans a user 
  - /sban: silently bans a user
  - /kick: kicks a user
  - /mute: mute a user
- Mute cmds
  - /tmute <d/h/m>: temporarily mute a user
  - /unmute: unmutes a user
- Warn cmds
  - /warn <userhandle>: warn a user
  - /resetwarn @username: reset the warnings for a user
  - /addwarn <word> <message>: set a warning filter on a certain word
