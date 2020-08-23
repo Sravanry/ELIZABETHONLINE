18b40011fab6727f8647fb7abadff6d40ac76527
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 11:49:32 +0000

    Update admin.py

--- alexa/modules/admin.py
+++ alexa/modules/admin.py
@@ -927,6 +927,7 @@ def reaction(bot: Bot, update: Update, args: List[str]) -> str:
             update.effective_message.reply_text("Reaction on admin commands for users now `enabled`!", parse_mode=ParseMode.MARKDOWN)
         
 __help__ = """
+*Advanced* 💡
  - /adminlist | /admins: list of admins in the chat
  - /users: list all the users in the chat
  - /pin | /unpin: pins/unpins the message in the chat
@@ -935,45 +936,69 @@ __help__ = """
  - /demote: demotes a user
  - /zombies: count the number of deleted account in your group
  - /kickthefools: kicks all members inactive from 1 week
+
+*Report to Admin* 📝
  - /report <reason> | @admin: reply to a message to report it to admins(non-admin only)
  - /reports <on/off>: change report setting
+
+*Punishment* 🚫
  - /ban: bans a user 
  - /tban <d/h/m> : temporarily bans a user from your chat
  - /unban: unbans a user 
  - /sban: silently bans a user
+
+*Mute* 🤬
  - /mute: mute a user 
  - /tmute <d/h/m>: temporarily mute a user
  - /unmute: unmutes a user
  - /kick: kicks a user 
+
+*Flood control* ⚠️
  - /setflood <number/off>: set the number of messages to take action on a user for flooding
  - /setfloodmode <mute/ban/kick/tban/tmute>: select the valid action eg. /setfloodmode tmute 5m.
  - /flood: gets the current antiflood settings
+
+*Black words* 🏴
  - /addblacklist <trigger> : blacklists the trigger
  - /unblacklist <trigger> | rmblacklist <trigger> : stop blacklisting a certain blacklist trigger
  - /blacklist: list all active blacklist filters
  - /addblacklist "the admins suck" respect your admins: This will remove the text everytime someone types it
  - /addblacklist `"bit.ly/*"`: This will remove the link everytime someone sends it matching `bit.ly`
+
+*Filters* 📑
  - /filter <word> <message>: Every time someone says "word", the bot will reply with "message"
  - /stop <word>: stop that filter.
  - /filters: list all active filters in this chat.
+
+*Connection* 🔄
  - /connection <chatid>: Connect to remote chat
  - /disconnect: disconnects a chat from pm
  - /allowconnect on/yes/off/no: allow connect users to group via PM
  - /helpconnect: gets the available list commands 
+
+*Locking* 🔐
  - /lock <item(s)>: lock the usage of "item" for non-admins
  - /unlock <item(s)>: unlock "item". Everyone can use them again
  - /locks: list the lock status in the chat
  - /locktypes: gets a list of all things that can be locked
+
+*Channel Log* 📹
  - /setlog: set a log channel.
  - /unsetlog: unset the log channel.
  - /logchannel: get the log channel info
+
+*Message Deletion* 🧺
  - /purge: deletes all messages from the message you replied to
  - /purge X: deletes X messages after the message you replied to 
  - /del: deletes the message you replied to.
+
+*Notes* 📚
  - /save <word> <sentence>: Save that sentence to the note called "word"
  - /get <word> | #<word> : get the note registered to that word
  - /clear <word>: delete the note called "word"
  - /notes | /saved: List all notes in the chat
+
+*Rules* ⚠️
  - /setrules <rules>: set the rules for this chat
  - /clearrules: clear the rules for this chat
  - /rules: get the rules for this chat
