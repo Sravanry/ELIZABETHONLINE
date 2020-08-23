f5f29e794aabcc8f4555ba4098c4e1b0b102703a
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 09:09:37 +0000

    Update assistant.py

--- alexa/modules/assistant.py
+++ alexa/modules/assistant.py
@@ -1,10 +1,9 @@
 __help__ = """
 *For text assistant*
- - /elizabeth <question>: Ask alexa any question and it will give accurate reply. For eg: `/alexa where is Taj Mahal`, `/alexa what is the age of Virat Kohli` etc..
+ - /elizabeth <question>: Ask Elizabeth any question and it will give accurate reply. For eg: `/elizabeth  where is Taj Mahal`, `/elizabeth what is the age of Virat Kohli` etc..
 *For voice assistant*
  - /elizabeth: Reply to a voice query and get the results in voice output
 
-*NOTE*
-The question should be a meaningful one otherwise you will get no response !
+⚠️ The question should be a meaningful one otherwise you will get no response !
 """
 __mod_name__ = "Assistant 🌐"
