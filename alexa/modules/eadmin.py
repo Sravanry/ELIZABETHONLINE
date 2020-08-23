81c12904d1eb9a2363ce9a3d6fe7829a4f8ccfbf
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 07:42:49 +0000

    Update eadmin.py

--- alexa/modules/eadmin.py
+++ alexa/modules/eadmin.py
@@ -10,7 +10,9 @@ __help__ = """
  - /kickthefools: kicks all members inactive from 1 week
  - /report <reason> | @admin: reply to a message to report it to admins(non-admin only)
  - /reports <on/off>: change report setting
-
+ - /setflood <number/off>: set the number of messages to take action on a user for flooding
+ - /setfloodmode <mute/ban/kick/tban/tmute>: select the valid action eg. /setfloodmode tmute 5m
+ - /flood: gets the current antiflood settings
 """
 
-__mod_name__ = "Admin👩‍✈️"
+__mod_name__ = "ADMIN👩‍✈️"
