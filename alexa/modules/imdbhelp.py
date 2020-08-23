356a5b297cc091f220010de07b2234f1b6977691
PRINCE <69641831+PRINCEONLINE@users.noreply.github.com>
2020-08-23 11:24:51 +0000

    Create imdbhelp.py

---
+++ alexa/modules/imdbhelp.py
@@ -0,0 +1,7 @@
+__help__ = """
+ - /imdb [query] Get imdb result from Imbd.com
+💡 For best result please search with year 
+Eg: `/imdb Titanic 1997`
+⚠️ You must be admin in group.. Otherwise us in *PM*. 
+"""
+__mod_name__ = "Imdb 🎬"
